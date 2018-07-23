7694277f140420a02b37c713e0149ab56070cf8a
Kyle Ellrott <kellrott@gmail.com>
2018-07-23 10:19:00 -0700

    Working on one-class classifier

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@ from Cython.Build import cythonize
 from distutils.extension import Extension
 import numpy
 
-extensions = [Extension("py_gelnet", 
-                ["py_gelnet.pyx", "gelnet.c"], 
+extensions = [Extension("py_gelnet",
+                ["py_gelnet.pyx", "gelnet.c"],
                 include_dirs=[numpy.get_include()] )
              ]
 
 setup(
+    name="py_gelnet",
     ext_modules = cythonize(extensions)
 )
